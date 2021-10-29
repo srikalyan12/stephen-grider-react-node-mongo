75eed237517e8c23d2412dac27b4cfc2785e22ef
srikalyan ranganathan <srikalyan.rs@gmail.com>
2021-10-30 01:10:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(typeof req.headers.cookie);
+  res.cookie('name', 'srikalyan').send('welcome');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('listening to port 5000'));
