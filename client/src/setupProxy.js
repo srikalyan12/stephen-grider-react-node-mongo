77403e9d03ddbb2c065a9a2cb7fb202c69094ea8
srikalyan ranganathan <srikalyan.rs@gmail.com>
2021-11-24 20:38:52 +0530

    adding setupproxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,17 +2,9 @@ const createProxyMiddleware = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/auth',
+    ['/auth/google', '/api'],
     createProxyMiddleware({
       target: 'http://localhost:5000',
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
-      changeOrigin: true,
     })
   );
 };
